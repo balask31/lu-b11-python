df582c764e2831838ed632d6fd18b20d6f0fa62c
balask31 <78898896+balask31@users.noreply.github.com>
2021-02-11 13:14:27 +0530

    Create sakthi.py

---
+++ sakthi.py
@@ -0,0 +1,11 @@
+altitude=400
+
+
+
+if altitude > 5000:
+    print("not safe and try it later")
+
+elif altitude>1100 or altitude>=5000:
+    print(" cpme 1000ft to land")
+else:
+    print("safe to land")
